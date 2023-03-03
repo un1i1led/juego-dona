c5b0d8cd630731ea7d3cdf6a2001738872713d42
gil <gsqwow@gmail.com>
2023-03-03 10:54:35 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [bestScore, setBestScore] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const updateState = (state, value) => {
+    if (state == 'score') {
+      setScore(value);
+      if (bestScore <= value) {
+        setBestScore(value);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
     </div>
-  );
+  )
+
 }
 
 export default App;

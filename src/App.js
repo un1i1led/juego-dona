a5243da7acaa460c608308cf669ac2d0e1075134
gil <gsqwow@gmail.com>
2023-03-13 19:54:51 -0700

    Add information divs and content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import GameContainer from './components/GameContainer';
+import Nav from './components/Nav';
 import TopBar from './components/Topbar';
 import './style.css';
 
@@ -18,10 +19,26 @@ const App = () => {
 
   return (
     <div className='App'>
+      <Nav/>
       <TopBar score={score} bestScore={bestScore}/>
       <div className='main'>
         <GameContainer score={score} updateState={updateState}/>
       </div>
+      <div className='info'>
+          <div className='somos info-div'>
+            <h2>¿Qué es DonaLoBien?</h2>
+            <p>Es un proyecto educativo que busca fomentar la cultura de la donación de ropa en buen estado. 
+              Nuestra misión es concientizar sobre la importancia de donar aquellos artículos que ya no utilizamos 
+              pero que aún pueden ser útiles para alguien más.</p>
+          </div>
+          <div className='funciona info-div'>
+            <h2>¿Cómo funciona el juego?</h2>
+            <p>El juego funciona de manera que se te presentan opciones de articulos de ropa que no estan en buen
+              estado y articulos de ropa que si lo estan, tu trabajo es escoger los articulos que si esten en 
+              buena condicion para donar e intentar obtener el puntaje mas alto que puedas.
+            </p>
+          </div>
+        </div>
     </div>
   )
 

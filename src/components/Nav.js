3c75e7250628079151dda145dd28b8bd6ad4c66b
gil <gsqwow@gmail.com>
2023-03-13 19:55:34 -0700

    Add nav component and structure

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <div className='nav-left'>
+                    <li>DonaLoBien</li>
+                </div>
+                <div className='nav-right'>
+                    <li>Bred</li>
+                    <li>Contactanos</li>
+                </div>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;

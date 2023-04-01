2ff3306909f25afe7d0c72c0350b64e48feb8cd9
gil <gsqwow@gmail.com>
2023-03-31 21:19:40 -0700

    Fix correct card generator from having more than one correct answer

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -6,33 +6,32 @@ const GameContainer = props => {
 
     const cardOrder = () => {
         const cards = [];
-        let validCard = false;
-
+        let validCardGenerated = false;
+      
         for (let i = 1; i <= 9; i++) {
-            const card = {
-                id: i,
-                valid: false
-            }
-
-            if (i === Math.floor(Math.random() * 9) + 1) {
-                card.valid = true;
-            }
-
-            cards.push(card);
-        }
-
-        if (!validCard) {
-            const randomIndex = Math.floor(Math.random() * 9);
-            cards[randomIndex].valid = true;
+          const card = {
+            id: i,
+            valid: false
+          };
+      
+          if (!validCardGenerated && Math.random() < 0.1) {
+            card.valid = true;
+            validCardGenerated = true;
           }
-
-        return (
-            cards.map((card) => {
-                return <Card key={card.id} id={card.id} valid={card.valid} changeArray={changeArray}/>
-            })
-        )
-    }
-
+      
+          cards.push(card);
+        }
+      
+        if (!validCardGenerated) {
+          const randomIndex = Math.floor(Math.random() * 9);
+          cards[randomIndex].valid = true;
+        }
+      
+        return cards.map((card) => {
+          return <Card key={card.id} id={card.id} valid={card.valid} changeArray={changeArray} />;
+        });
+      };
+      
     const changeArray = (valid) => {
         if (valid === false) {
             props.updateState('score', 0);
